678276d1cf670246fbb5b7434df01a3079944dd4
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-05-04 15:16:11 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,9 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  //   const {} = req.body;
+  const { title, genre } = req.body;
   console.log("files", req.files);
 
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
@@ -43,7 +45,20 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     console.log("bookFileUploadResult", bookFileUploadResult);
     console.log("uploadResult", uploadResult);
 
-    res.json({});
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "6634584f814e444a85a4e751",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // delete temp files
+    // todo wrap in trycatch
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({ id: newBook._id });
   } catch (err) {
     console.log(err);
     return next(createHttpError(500, "Error while uploading the files."));
